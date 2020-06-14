abc47cffd44843c9db57aa7dae8be5fc2a299c53
James Emselle <james.e.0111.2@gmail.com>
2020-06-14 21:50:06 +0200

    Fill.asm and mult.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,64 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+(WAITWHITE)
+    @24576
+    D=M
+    @PAINTBLACK
+    D;JNE
+
+    // Loop
+    @WAITWHITE
+    0;JMP
+
+
+(WAITBLACK)
+    @24576
+    D=M
+    @PAINTWHITE
+    D;JEQ
+
+    // Loop
+    @WAITBLACK
+    0;JMP
+
+(PAINTBLACK)
+    @8192
+    D=A
+
+(BLACK)
+    // Write 0xFFFF to 16384 + D
+    @16384
+    A=A+D
+    M=-1
+
+    // Jump to WAITBLACK if D <= 0
+    @WAITBLACK
+    D=D-1;JLT
+
+    // Loop
+    @BLACK
+    0;JMP
+
+(PAINTWHITE)
+    @8192
+    D=A
+
+(WHITE)
+    // Write 0xFFFF to 16384 + D
+    @16384
+    A=A+D
+    M=0
+
+    // Jump to WAITWHITE if D <= 0
+    @WAITWHITE
+    D=D-1;JLT
+
+    // Loop
+    @WHITE
+    0;JMP
+
+(END) // Infinite loop
+    @END
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0
+
+(LOOP)
+    // If R0 <= 0 then go to END
+    @R0
+    D=M
+    @END
+    D;JLE
+
+    // Reduce R0 by 1
+    @R0
+    M=M-1
+
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // Loop
+    @LOOP
+    0;JMP
+
+(END) // Infinite loop
+    @END
+    0;JMP
